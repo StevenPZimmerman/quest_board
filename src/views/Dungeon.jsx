3a8fe2f96f2d65104f34868133b304e1f560dfa3
Steven Zimmerman <stevenpzimmerman@protonmail.com>
2021-05-25 12:43:49 -0500

    shop is displaying

--- src/views/Dungeon.jsx
+++ src/views/Dungeon.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "../client/scss/App.scss";
 
 const Dungeon = () => {
+
+  
+    const [shop, setShop] = React.useState([]);
+  
+    React.useEffect(() => {
+      fetch("/shop")
+        .then((res) => res.json())
+        .then((data) => setShop(data));
+    }, []);
+  
+    let shopList = 0;
+  
+
   return (
     <div>
-      <p>Dungeon</p>
+    {shop.map((equip) => {
+      if (equip.Equipped == 0 && shopList < 4) {
+        shopList++
+        
+        return (
+        
+<div
+          className="card border-dark mb-3"
+          style={{ width: "18rem", marginTop: "2rem", marginLeft: "2rem" }}
+          >
+          <div className="card-body">
+            <h3 className="card-title">{equip.EquipName}</h3>
+            <h5 className="card-title">{equip.Price}</h5>
+            <p>{equip.Equipped}</p>
+          </div>
+        </div>)
+
+        
+      }
+    })}
     </div>
   );
 };
--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -2,45 +2,39 @@ import React from "react";
 import "../client/scss/App.scss";
 
 const Shop = () => {
-  const [shop, setShop] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("/shop")
-      .then((res) => res.json())
-      .then((data) => setShop(data));
-  }, []);
-
-  //   shop.map((equip) => {
-  //     if (equip.Equipped == 0) {
-
-  //         <div className="card border-dark mb-3" style={{width: '18rem', marginTop: '2rem', marginLeft: '2rem'}}>
-  //         <div className="card-body">
-  //         <h3 className="card-title">{equip.EquipName}</h3>
-  //         <h5 className="card-title">{equip.Price}</h5>
-
-  //         </div>
-
-  //     </div>
-
-  //     }
-  //   });
+    const [shop, setShop] = React.useState([]);
+  
+    React.useEffect(() => {
+      fetch("/shop")
+        .then((res) => res.json())
+        .then((data) => setShop(data));
+    }, []);
+  
+    let shopList = 0;
+  
 
   return (
     <div>
-      {shop.map((equip) => (
-        <div>
-          <div
-            className="card border-dark mb-3"
-            style={{ width: "18rem", marginTop: "2rem", marginLeft: "2rem" }}
+    {shop.map((equip) => {
+      if (equip.Equipped == 0 && shopList < 4) {
+        shopList++
+        
+        return (
+        
+<div
+          className="card border-dark mb-3"
+          style={{ width: "18rem", marginTop: "2rem", marginLeft: "2rem" }}
           >
-            <div className="card-body">
-              <h3 className="card-title">{equip.EquipName}</h3>
-              <h5 className="card-title">{equip.Price}</h5>
-              <p></p>
-            </div>
+          <div className="card-body">
+            <h3 className="card-title">{equip.EquipName}</h3>
+            <h5 className="card-title">{equip.Price}</h5>
+            <p>{equip.Equipped}</p>
           </div>
-        </div>
-      ))}
+        </div>)
+
+        
+      }
+    })}
     </div>
   );
 };
